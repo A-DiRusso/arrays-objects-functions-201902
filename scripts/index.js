716458dab7ce1e23018de67b54687bf97ee6a2c7
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 14:05:42 -0500

    adds  function

--- scripts/index.js
+++ scripts/index.js
@@ -23,6 +23,16 @@ function noOdds(arr) {
 
 noOdds([1, 2, 3, 4, 5, 6, 7, 8])
 
+//************square numbers*********
+function squaresVille(flats) {
+    let bigs = flats.map((val, i, flats) => {
+        return val * val;
+    });
+    console.log(bigs);
+}
+
+squaresVille([2, 5, 6, 7, 99]);    
+
 
 
 
