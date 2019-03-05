a0f643643104a2305a710b3feb943baf6fefc8b7
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 15:52:44 -0500

    adds sort by price

--- scripts/index.js
+++ scripts/index.js
@@ -139,5 +139,20 @@ let longName = people.sort(function(a, b){
 
 console.log(longName);
 
+// ********************sort by price**********************
+
+var products = [
+    { name: 'Basketball', price: 12.00 },
+    { name: 'Tennis Racquet', price: 66.00 },
+    { name: 'Tennis Balls', price: 9.00 },
+    { name: 'Tennis Balls', price: 9.00 }
+  ];
+
+let highDollar = products.sort(function(a, b){
+    return b.price - a.price;
+});
+
+console.log(highDollar);
+
 
 
