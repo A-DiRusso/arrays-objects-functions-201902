daf614a09561996c7319ce02d0648e74d7533c63
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 13:55:01 -0500

    adds  function

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
 // add your solutions here
 
 //********postive numbers ********
+function noNegitive(numbArrs) {
+    let greaterThan = numbArrs.filter(val => {
+        return val > 0;
+    });
+    console.log(greaterThan);
+}
 
+noNegitive([1, -2, 3, -5, 9]);
+
+
+
+
+//**********even numbers *********
 function noOdds(arr) {
     let even = arr.filter(val => {
         return val % 2 === 0;
@@ -10,5 +22,9 @@ function noOdds(arr) {
 }
 
 noOdds([1, 2, 3, 4, 5, 6, 7, 8])
+
+
+
+
 
 
