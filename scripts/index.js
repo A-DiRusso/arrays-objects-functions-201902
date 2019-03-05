dddd7ac3ff41eb00b7f38349eafd784a9b3291c2
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 13:42:26 -0500

    completes function noOdds

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 // add your solutions here
+
+//********postive numbers ********
+
+function noOdds(arr) {
+    let even = arr.filter(val => {
+        return val % 2 === 0;
+    });
+    console.log(even);
+}
+
+noOdds([1, 2, 3, 4, 5, 6, 7, 8])
+
+
